bfb1a1b345690e493a308a7b07d0c6778ad239bc
JameyWoo <2622075127wjh@gmail.com>
2020-03-26 20:18:00 +0800

    update command: ls, set-name, default

--- client/client.go
+++ client/client.go
@@ -1,19 +1,35 @@
+/*
+协议:
+用户可以执行命令, 以  %<command> args... 的形式
+如:
+	%set-name 姬小野
+	%get-file 十万个为什么.pdf
+可以直接输入消息, 也就是除合法命令格式以外的消息, 都作为发送到chatroom的消息来发送
+
+由于tcp传输字节流, 因此在发送每个消息之前, 用一个 4字节 的数据声明要发送的消息的长度, 避免粘包.
+
+*/
+
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// 需要指定server的 ip 端口号, 以及自己的用户名
-	if len(os.Args) != 4 {
-		fmt.Printf("Usage : %s <ip> <port> <user_name>\n", os.Args[0])
+	// 需要指定server的 ip 端口号
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage : %s <ip> <port>\n", os.Args[0])
 		os.Exit(1)
 	}
-	conn, err := net.Dial("tcp", os.Args[1] + ":" + os.Args[2])
+	conn, err := net.Dial("tcp", os.Args[1]+":"+os.Args[2])
 	defer conn.Close()
 	if err != nil {
 		panic(err)
@@ -23,12 +39,68 @@ func main() {
 		// 将 conn 中的数据拷贝到 os.Stdout. 文件的拷贝也可以这样实现.
 		// 这是将 conn读取的数据输出的意思.
 		// ! 并且这个协程应该会一直读取, 而不会终止
-		io.Copy(os.Stdout, conn)
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil {
+			panic(err)
+		}
 		// 服务器断开连接的时候, 才会往下执行
 		done <- struct{}{}
 	}()
-	if _, err := io.Copy(conn, os.Stdin); err != nil {
-		panic(err)
+	// 这里不能使用 io.Copy 函数, 因为需要解析命令
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		inputStr := input.Text()
+		// 将输入解析, 如果是命令的格式, 那么以命令的方式传递
+		subInput := strings.Fields(inputStr)
+		switch subInput[0] {
+		// 在这里只需要 upload-file, get-file 是需要上下文操作的, 其他的命令只需要传递过去让server处理
+		case "%upload-file": // 上传文件
+
+		case "%get-file": // 下载文件
+
+		case "%exit": // 退出聊天室
+			return
+
+		case "%set-name": // 设置用户的名字
+			fallthrough
+		case "%ls": // 列出聊天室中所有的文件
+			fallthrough
+		default:
+			// 先计算数据长度, 然后拼接
+			length := len([]byte(inputStr))
+			preSend := BytesCombine(BytesCombine(IntToBytes(length)), []byte(inputStr))
+			_, err := conn.Write(preSend)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
+	//if _, err := io.Copy(conn, os.Stdin); err != nil {
+	//	panic(err)
+	//}
 	<-done
+}
+
+// 合并两个 []byte
+func BytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(""))
+}
+
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
--- server/server.go
+++ server/server.go
@@ -1,12 +1,16 @@
 // 不错的博客: https://blog.csdn.net/yjp19871013/article/details/82711237
+// TODO: 发现一个bug, 用 panic 会造成server终止, 当client的连接中断时, 应该忽略错误
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 // 控制客户端输出的channel
@@ -28,12 +32,10 @@ func broadcaster() {
 		case msg := <-messages:
 			// Broadcast incoming message to all
 			// clients' outgoing message channels.
-			// ? 但是msg 不是只能 <- 给一个 cli 吗? 给了他就空了呀, 怎么做到给所有的cli的?
 			// ! 注意, 这里msg是字符串, 而不是 chan !!! 所以可以给每个 cli 赋值!!!
 			// 打印服务端信息
 			fmt.Println(msg)
 			for cli := range clients {
-				// ? 这里搞不懂为什么会把广播输出给所有的客户端? 怎么输出的?
 				// ! 会继续执行阻塞的 clientWriter 函数
 				cli <- msg
 			}
@@ -56,23 +58,75 @@ func handleConn(conn net.Conn) {
 	go clientWriter(conn, ch)
 
 	who := conn.RemoteAddr().String()
+	var alias string  // 用户自定义的别名
 	ch <- "You are " + who
 	// * 要被广播的message, broadcaster 会捕捉
 	messages <- who + " has arrived"
 	entering <- ch // 表示新的用户进入了
 
-	// ! 读取网络输入, 然后传递给message用以广播
-	input := bufio.NewScanner(conn)
-	// .Scan() 函数为true时, 代表在输入, 否则代表不输入了(应该是文件关闭了)
-	// ! 这里实现的是服务端读取客户端的网络输入, 并将其传递给messages用以广播
-	for input.Scan() {
-		messages <- who + ": " + input.Text()
+	for {
+		// 先读取 4 字节, 作为长度
+		lengthByte := make([]byte, 4)
+		length, _ := conn.Read(lengthByte)  // 忽略错误
+		if length != 4 {  // 读取 int 类型的长度
+			continue
+		}
+		length = BytesToInt(lengthByte)  // int 类型的长度
+		inputByte := make([]byte, length) // 输入命令
+		length, _ = conn.Read(inputByte)  // 忽略错误
+		inputStr := string(inputByte)
+		// 将输入解析, 如果是命令的格式, 那么以命令的方式传递
+		subInput := strings.Fields(inputStr)
+
+		// 分别处理每一个命令
+		switch subInput[0] {
+		// 在这里只需要 upload-file, get-file 是需要上下文操作的, 其他的命令只需要传递过去让server处理
+		case "%upload-file": // 上传文件
+
+		case "%get-file": // 下载文件
+
+		case "%set-name": // 设置用户的名字
+			if len(subInput) >= 2 {  // 取 %set-name 之后的第一个字符串为名字
+				alias = subInput[1]
+				ch <-"设置用户名 \"" + alias + " \"成功!"
+			} else {
+				ch <-"设置用户名失败! 请输入合法的用户名!"
+			}
+		case "%ls": // 列出聊天室中所有的文件
+			// TODO: 要注意这里如果在其他目录运行server.go, 这个目录还是否有效
+			files, err := ioutil.ReadDir("./disk")
+			if err != nil {
+				panic(err)
+			}
+			var filesStr string
+			for _, file := range files {
+				filesStr += file.Name() + "\t"
+			}
+			// 这里是单独给当前客户端发送信息
+			ch <-filesStr
+		default:
+			// 广播信息
+			if len(alias) > 0 {
+				messages <- who + "(" + alias + ")" +  ": " + inputStr
+			} else {
+				messages <- who + ": " + inputStr
+			}
+		}
 	}
+
+	//// ! 读取网络输入, 然后传递给message用以广播
+	//input := bufio.NewScanner(conn)
+	//// .Scan() 函数为true时, 代表在输入, 否则代表不输入了(应该是文件关闭了)
+	//// ! 这里实现的是服务端读取客户端的网络输入, 并将其传递给messages用以广播
+	//for input.Scan() {
+	//
+	//	messages <- who + ": " + input.Text()
+	//}
 	// NOTE: ignoring potential errors from input.Err()
 
 	// ! .Scan() 为false之后, 不再输入, 说明socket断开连接, 告知离开
 	leaving <- ch
-	messages <- who + " has left"
+	messages <- who + alias + " has left"
 	conn.Close()
 }
 
@@ -106,4 +160,23 @@ func main() {
 		}
 		go handleConn(conn)
 	}
+}
+
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
